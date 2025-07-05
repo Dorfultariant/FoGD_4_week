ecbfbb78aa0692add9a28ed5afeeb58be74c8721
Dorfultariant <tunxero@hotmail.com>
2025-07-05 11:26:01 +0300

    more null checking

--- Scripts/level_handler.gd
+++ Scripts/level_handler.gd
@@ -2,20 +2,23 @@ extends Node2D
 
 class_name Level
 
-@onready var coins = $Coins.get_children()
+var coins
+
+@onready var spikes 
 signal CoinsCollected()
 
 var level_complete : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if not level_complete:
-		coins = $Coins.get_children()
-		if len(coins) <= 0:
-			CoinsCollected.emit()
-			level_complete = true
+		if $Coins:
+			coins = $Coins.get_children()
+			if len(coins) <= 0:
+				CoinsCollected.emit()
+				level_complete = true
