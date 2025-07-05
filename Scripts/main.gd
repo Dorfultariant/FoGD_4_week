2892fc460c5c60de966af10711965bc084b713d2
Dorfultariant <tunxero@hotmail.com>
2025-07-05 11:21:05 +0300

    Level null checking

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -9,19 +9,19 @@ var cur_lvl_idx = 0
 # Scene start
 func _ready() -> void:
 	print("Ready called")
-	cur_lvl_node = levels[cur_lvl_idx].instantiate()
-	add_child(cur_lvl_node)
-	cur_lvl_node.CoinsCollected.connect(change_level)
-	
+	if levels[cur_lvl_idx]:
+		cur_lvl_node = levels[cur_lvl_idx].instantiate()
+		add_child(cur_lvl_node)
+		cur_lvl_node.CoinsCollected.connect(change_level)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 func change_level() -> void:
-	print(levels.size(), ":", cur_lvl_idx)
-	if levels.size() - 1 == cur_lvl_idx :
-		print("End of the line")
+	# If the last level is reached, do nothing
+	if levels.size() - 1 == cur_lvl_idx:
 		return
 	cur_lvl_node.queue_free()
 	cur_lvl_idx += 1
